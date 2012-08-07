c85965892ccbb11f7e767bd2ab7fa92137895e54
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2012-08-07 14:24:02 -0400

    Implement: cas, get_from_last, prepend, quit, replace, server_by_key, set_servers, set_prefix_key

--- lib/fakememcached.rb
+++ lib/fakememcached.rb
@@ -2,27 +2,35 @@ require 'fakememcached/cache_entry'
 
 class FakeMemcached
   def initialize(servers = nil, opts = {})
-    @servers = servers
     @default_ttl = opts[:default_ttl] || 1_000_000
     @data = {}
+    set_servers(servers)
+    set_prefix_key(opts[:prefix_key] || opts[:namespace])
   end
 
-  # NOT IMPLEMENTED:
-  # * cas
-  # * get_from_last
-  # * prepend
-  # * quit
-  # * replace
-  # * server_by_key
-  # * set_servers
-  # * set_prefix_key
+  def set_servers(servers)
+    @servers = servers
+  end
+
+  def servers
+    @servers || []
+  end
+
+  def set_prefix_key(prefix_key)
+    @prefix_key = prefix_key
+  end
+  alias :set_namespace :set_prefix_key
 
   def prefix_key
-    ""
+    @prefix_key || ''
   end
   alias :namespace :prefix_key
   
-  def set(key, value, ttl = nil, marshal = true, flags = nil)
+  def reset(current_servers = nil)
+    set_servers(current_servers) if current_servers
+  end
+
+  def set(key, value, ttl = @default_ttl, marshal = true, flags = nil)
     @data[key] = CacheEntry.new(value, ttl.nil? || ttl.zero? ? @default_ttl : ttl, marshal)
   end
 
@@ -33,6 +41,67 @@ class FakeMemcached
       set(key, value, ttl, marshal, flags)
       "STORED\r\n"
     end
+  end
+
+  def increment(key, offset = 1)
+    if has_unexpired_key?(key)
+      @data[key].increment(offset)
+      @data[key].to_i
+    end
+  end
+
+  def decrement(key, amount = 1)
+    if has_unexpired_key?(key)
+      @data[key].decrement(amount)
+      @data[key].to_i
+    end
+  end
+
+  alias :incr :increment
+  alias :decr :decrement
+
+  def replace(key, value, ttl = @default_ttl, marshal = true, flags = nil)
+    if @data.include?(key)
+      set(key, value, ttl, marshal, flags)
+    else
+      raise Memcached::NotFound
+    end
+  end
+
+  def append(key, value)
+    set(key, get(key, false).to_s + value.to_s, nil, false)
+  end
+
+  def prepend(key, value)
+    set(key, value.to_s + get(key, false).to_s, nil, false)
+  end
+
+  def cas(key, ttl = @default_ttl, marshal = true, flags = nil)
+    value = get(key, marshal)
+    new_value = yield(value)
+    if get(key, marshal) == value
+      set(key, new_value, ttl, marshal, flags)
+    else
+      raise Memcached::NotStored
+    end
+  end
+  alias :compare_and_swap :cas
+  
+  def delete(key)
+    @data.delete(key) if has_unexpired_key?(key)
+  end
+
+  def flush
+    @data.clear
+  end
+  alias :flush_all :flush
+
+  def reset(current_servers = nil)
+    @servers = current_servers
+  end
+
+  def quit
+    self
   end
 
   def get(key, marshal = true)
@@ -48,42 +117,10 @@ class FakeMemcached
       end
     end
   end
+  alias :get_from_last :get
 
-  def increment(key, offset = 1)
-    if has_unexpired_key?(key)
-      @data[key].increment(offset)
-      @data[key].to_i
-    end
-  end
-  alias :incr :increment
-
-  def decrement(key, amount = 1)
-    if has_unexpired_key?(key)
-      @data[key].decrement(amount)
-      @data[key].to_i
-    end
-  end
-  alias :decr :decrement
-
-  def append(key, value)
-    set(key, get(key, false).to_s + value.to_s, nil, false)
-  end
-  
-  def delete(key)
-    @data.delete(key) if has_unexpired_key?(key)
-  end
-
-  def servers
-    @servers
-  end
-
-  def flush
-    @data.clear
-  end
-  alias :flush_all :flush
-
-  def reset(current_servers = nil)
-    @servers = current_servers
+  def server_by_key(key)
+    servers.first
   end
 
   def stats(subcommand = nil)
